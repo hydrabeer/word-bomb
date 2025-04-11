8c054aee946f68b44e0eb9bb2b4bb70bbb44a619
hydrabeer <65182259+hydrabeer@users.noreply.github.com>
2025-04-11 03:09:52 -0400

    Add clarifying comment

--- apps/frontend/src/vite-env.d.ts
+++ apps/frontend/src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+// This is not actually unused; socket.ts is using it to infer type information about
+// VITE_BACKEND_URL
 interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
 }
