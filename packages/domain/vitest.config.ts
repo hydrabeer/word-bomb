31bafb18604b2bcf0e542fb961762ce5be4dbe73
Zachary Muir <65182259+hydrabeer@users.noreply.github.com>
2025-09-27 15:53:17 -0400

    Update vitest conf in domain to have proper exclusions

--- packages/domain/vitest.config.ts
+++ packages/domain/vitest.config.ts
@@ -3,12 +3,25 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
+    include: ['src/**/*.{test,spec}.ts', 'test/**/*.{test,spec}.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/coverage/**',
+      '**/.{git,cache,output,temp}/**',
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'lcov'],
+      include: ['src/**/*.{ts,tsx}'],
       exclude: [
-        // barrels
-        'packages/**/src/index.ts',
+        '**/dist/**',
+        '**/*.d.ts',
+        'vitest.config.*',
+        'test/**',
+        'src/**/__tests__/**',
+        'src/**/testutils/**',
+        'src/index.ts',
       ],
     },
   },
