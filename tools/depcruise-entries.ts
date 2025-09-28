c1788355e7fae0d2b2780fdff56e5664755fd76e
hydrabeer <65182259+hydrabeer@users.noreply.github.com>
2025-09-28 14:54:06 -0400

    Delete scripts I won't use

--- tools/depcruise-entries.ts
+++
@@ -1,7 +0,0 @@
-// entrypoints for dependency-cruiser to analyze the workspace graph.
-export default [
-  'apps/frontend/src/main.tsx',
-  'apps/backend/src/index.ts',
-  'packages/types/src/index.ts',
-  'packages/domain/src/index.ts',
-];
